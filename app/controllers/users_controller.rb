d6bcdf84314e49ea06427a07c263ac3f470120cb
Wendy Xu <guangw.xu@gmail.com>
2016-09-08 17:03:21 -0400

    added logout, fixed password authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,50 @@
 class GifsController < ApplicationController
 
-   get '/login' do
-     erb :'users/login'
-   end
-
-   post '/login' do
-     @user = User.find_by(username: params[:username])
-     redirect ("/gifs")
-   end
-
    get '/signup' do
-     erb :'users/signup'
+    if logged_in?
+      redirect '/'
+    else
+      erb :'users/signup'
+    end
    end
 
    post '/signup' do
-     @user = User.create(params)
-     redirect ("/login")
+    user = User.new(params)
+    if user.save && (user.username != '') && (user.email != '')
+      session[:user_id] = user.id
+      redirect '/'
+    else
+      redirect '/signup'
+    end
    end
+
+   get '/login' do
+    if logged_in?
+      redirect '/gifs'
+    else
+     erb :'users/login'
+    end
+   end
+
+   post '/login' do
+    @user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/tweets'
+    else
+      redirect ("/gifs")
+    end
+   end
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+      redirect '/login'
+    else
+      redirect '/'
+    end
+  end
+
 
 
 end
