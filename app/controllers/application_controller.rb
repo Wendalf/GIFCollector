7911d2a473580f2cf38eb1b4e251cde0496e4964
Wendy Xu <guangw.xu@gmail.com>
2016-09-09 15:12:25 -0400

    moved links to layout page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < Sinatra::Base
   get '/' do
   #  binding.pry
     @gifs = Gif.all
-      @user = User.find_by_id(session[:user_id])
+    @user = User.find_by_id(session[:user_id])
     erb :index
   end
 
