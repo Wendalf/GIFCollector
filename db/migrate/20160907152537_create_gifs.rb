1b03450a9ef6f7f16e7149f3c0c15fcde7031c48
Wendy Xu <guangw.xu@gmail.com>
2016-09-10 16:28:55 -0400

    remove db from git tracking

--- db/migrate/20160907152537_create_gifs.rb
+++
@@ -1,9 +0,0 @@
-class CreateGifs < ActiveRecord::Migration
-  def change
-    create_table :gifs do |t|
-    	t.string :filename
-    	t.string :description
-    	t.integer :user_id
-    end
-  end
-end
--- db/migrate/20160907152753_create_gif_tags.rb
+++
@@ -1,8 +0,0 @@
-class CreateGifTags < ActiveRecord::Migration
-  def change
-    create_table :gif_tags do |t|
-      t.integer :gif_id
-      t.integer :tag_id
-    end
-  end
-end
