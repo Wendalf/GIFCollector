f8ab00fb78bd623499e4e307d239013b1cd90f0f
Wendy Xu <guangw.xu@gmail.com>
2016-09-10 22:19:14 -0400

    fixed gitignore gile, added migration files back

---
+++ db/migrate/20160911021626_create_gifs.rb
@@ -0,0 +1,9 @@
+class CreateGifs < ActiveRecord::Migration
+  def change
+  	create_table :gifs do |t|
+    	t.string :filename
+    	t.string :description
+    	t.integer :user_id
+    end
+  end
+end
